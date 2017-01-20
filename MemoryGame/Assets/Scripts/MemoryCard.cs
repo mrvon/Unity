152aebac91a773acc6b7c4f85c7d93b4c3b490a4
dennis <mrvon@qq.com>
2017-01-20 11:48:13 +0800

    update

---
+++ MemoryGame/Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryCard : MonoBehaviour {
+    [SerializeField]
+    private SceneController controller;
+    private int _id;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private int id {
+        get { return _id;  }
+    }
+
+    public void SetCard(int id, Sprite image) {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+}
---
+++ MemoryGame/Assets/Scripts/SceneController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour {
+    public const int gridRows = 2;
+    public const int gridCols = 4;
+    public const float offsetX = 2f;
+    public const float offsetY = 2.5f;
+
+    [SerializeField]
+    private MemoryCard originalCard = null;
+    [SerializeField]
+    private Sprite[] images = null;
+
+    // Use this for initialization
+    void Start () {
+        Vector3 startPos = originalCard.transform.position;
+
+        int[] numbers = { 0, 0, 1, 1, 2, 2, 3, 3 };
+        numbers = ShuffleArray(numbers);
+
+        for (int i = 0; i < gridCols; i++) {
+            for (int j = 0; j < gridRows; j++) {
+                MemoryCard card;
+                if (i == 0 && j == 0) {
+                    card = originalCard;
+                } else {
+                    card = Instantiate(originalCard) as MemoryCard;
+                }
+
+                int index = j * gridCols + i;
+                int id = numbers[index];
+                card.SetCard(id, images[id]);
+
+                float posX = (offsetX * i) + startPos.x;
+                float posY = -(offsetY * j) + startPos.y;
+                card.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
